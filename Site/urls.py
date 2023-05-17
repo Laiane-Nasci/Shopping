4383efd870b5ff76683b08327dc4ad4f1a2dedc3
Laiane Nascimento <laiane_ns@hotmail.com>
2023-05-17 20:46:16 -0300

    viws e urls

--- Site/urls.py
+++ Site/urls.py
@@ -3,6 +3,6 @@ from Site import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('produtos', views.produto.lista, name='produto_lista'),
+    path('produtos', views.produto_lista, name='produto_lista'),
     path('produto', views.produto_detalhe, name= 'produto_detalhe')
 ]
